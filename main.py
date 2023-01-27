3eb1418518781e0fd1c94a8b7660a70bd7c8f329
sabin <sabina190706@gmail.com>
2023-01-27 13:30:08 +0600

    Прототипная версия. Некоторые баги возможны.

---
+++ main.py
@@ -0,0 +1,228 @@
+import argparse
+import os
+import time
+from threading import Thread
+
+import cv2
+import dlib
+import imutils
+import numpy as np
+from imutils import face_utils
+from imutils.video import VideoStream
+from scipy.spatial import distance as dist
+
+
+def alarm(msg):
+    global alarm_status
+    global alarm_status2
+    global saying
+
+    while alarm_status:
+        print('call')
+        s = 'espeak "' + msg + '"'
+        os.system(s)
+
+    if alarm_status2:
+        print('call')
+        saying = True
+        s = 'espeak "' + msg + '"'
+        os.system(s)
+        saying = False
+
+
+def eye_aspect_ratio(eye):
+    A = dist.euclidean(eye[1], eye[5])
+    B = dist.euclidean(eye[2], eye[4])
+
+    C = dist.euclidean(eye[0], eye[3])
+
+    ear = (A + B) / (2.0 * C)
+
+    return ear
+
+
+def final_ear(shape):
+    (lStart, lEnd) = face_utils.FACIAL_LANDMARKS_IDXS["left_eye"]
+    (rStart, rEnd) = face_utils.FACIAL_LANDMARKS_IDXS["right_eye"]
+
+    leftEye = shape[lStart:lEnd]
+    rightEye = shape[rStart:rEnd]
+
+    leftEAR = eye_aspect_ratio(leftEye)
+    rightEAR = eye_aspect_ratio(rightEye)
+
+    ear = (leftEAR + rightEAR) / 2.0
+    return (ear, leftEye, rightEye)
+
+
+def lip_distance(shape):
+    top_lip = shape[50:53]
+    top_lip = np.concatenate((top_lip, shape[61:64]))
+
+    low_lip = shape[56:59]
+    low_lip = np.concatenate((low_lip, shape[65:68]))
+
+    top_mean = np.mean(top_lip, axis=0)
+    low_mean = np.mean(low_lip, axis=0)
+
+    distance = abs(top_mean[1] - low_mean[1])
+    return distance
+
+
+K = [6.2500000000000000e+002, 0.0, 3.1250000000000000e+002,
+     0.0, 6.2500000000000000e+002, 3.1250000000000000e+002,
+     0.0, 0.0, 1.0]
+
+# 3D model points.
+model_points = np.array([
+    (0.0, 0.0, 0.0),  # Nose tip
+    (0.0, -330.0, -65.0),  # Chin
+    (-225.0, 170.0, -135.0),  # Left eye left cornerq
+    (225.0, 170.0, -135.0),  # Right eye right corner
+    (-150.0, -150.0, -125.0),  # Left Mouth corner
+    (150.0, -150.0, -125.0)  # Right mouth corner
+
+])
+
+ap = argparse.ArgumentParser()
+ap.add_argument("-w", "--webcam", type=int, default=0,
+                help="index of webcam on system")
+args = vars(ap.parse_args())
+
+EYE_AR_THRESH = 0.3
+EYE_AR_CONSEC_FRAMES = 30
+YAWN_THRESH = 20
+alarm_status = False
+alarm_status2 = False
+saying = False
+COUNTER = 0
+TOTAL=0
+start_time=time.time()
+print("-> Loading the predictor and detector...")
+detector = cv2.CascadeClassifier("haarcascade_frontalface_default.xml")
+predictor = dlib.shape_predictor('shape_predictor_68_face_landmarks.dat')
+
+print("-> Starting Video Stream")
+vs = VideoStream(src=args["webcam"]).start()
+time.sleep(1.0)
+
+while True:
+
+    frame = vs.read()
+    frame = imutils.resize(frame, width=450)
+    gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+    rects = detector.detectMultiScale(gray, scaleFactor=1.1,
+                                      minNeighbors=5, minSize=(30, 30),
+                                      flags=cv2.CASCADE_SCALE_IMAGE)
+
+    cam_matrix = np.array(K).reshape(3, 3).astype(np.float32)
+    for face in rects:
+        x1 = face.left()
+        y1 = face.top()
+        x2 = face.right()
+        y2 = face.bottom()
+        # Then we can also do cv2.rectangle function (frame, (x1, y1), (x2, y2), (0, 255, 0), 3)
+        landmarks = predictor(gray, face)
+
+        # We are then accesing the landmark points
+        i = [33, 8, 36, 45, 48,
+             54]  # Nose tip, Chin, Left eye corner, Right eye corner, Left mouth corner, right mouth corner
+        image_points = []
+        for n in i:
+            x = landmarks.part(n).x
+            y = landmarks.part(n).y;
+            # image_points = np.array([(x,y)], dtype="double")
+            image_points += [(x, y)]
+            cv2.circle(frame, (x, y), 2, (255, 255, 0), -1)
+
+        image_points = np.array(image_points, dtype="double")
+        # print(image_points)
+        print("Camera Matrix :\n {0}".format(cam_matrix))
+
+        dist_coeffs = np.zeros((4, 1))  # Assuming no lens distortion
+        (success, rotation_vector, translation_vector) = cv2.solvePnP(model_points, image_points,
+                                                                      cam_matrix, dist_coeffs,
+                                                                      flags=cv2.SOLVEPNP_ITERATIVE)
+
+        print("Rotation Vector:\n {0}".format(rotation_vector))
+        print("Translation Vector:\n {0}".format(translation_vector))
+
+        # Project a 3D point (0, 0, 1000.0) onto the image plane.
+        # We use this to draw a line sticking out of the nose
+
+        (nose_end_point2D, jacobian) = cv2.projectPoints(np.array([(0.0, 0.0, 1000.0)]), rotation_vector,
+                                                         translation_vector,
+                                                         cam_matrix, dist_coeffs)
+
+        for p in image_points:
+            cv2.circle(frame, (int(p[0]), int(p[1])), 3, (0, 0, 255), -1)
+
+        p1 = (int(image_points[0][0]), int(image_points[0][1]))
+        p2 = (int(nose_end_point2D[0][0][0]), int(nose_end_point2D[0][0][1]))
+
+        cv2.line(frame, p1, p2, (255, 0, 0), 2)
+#for rect in rects:
+    for (x, y, w, h) in rects:
+        rect = dlib.rectangle(int(x), int(y), int(x + w), int(y + h))
+        shape = predictor(gray, rect)
+        shape = face_utils.shape_to_np(shape)
+
+        eye = final_ear(shape)
+        ear = eye[0]
+        leftEye = eye[1]
+        rightEye = eye[2]
+        distance = lip_distance(shape)
+
+        leftEyeHull = cv2.convexHull(leftEye)
+        rightEyeHull = cv2.convexHull(rightEye)
+        cv2.drawContours(frame, [leftEyeHull], -1, (0, 255, 0), 1)
+        cv2.drawContours(frame, [rightEyeHull], -1, (0, 255, 0), 1)
+
+        lip = shape[48:60]
+        cv2.drawContours(frame, [lip], -1, (0, 255, 0), 1)
+        end_time=time.time()
+        if end_time-start_time>60.0: #если прошло 60 секунд то обновляем счетчик морганий
+            start_time=end_time
+            TOTAL=0
+
+        if ear < EYE_AR_THRESH:
+            COUNTER += 1
+
+            if COUNTER >= EYE_AR_CONSEC_FRAMES:
+                TOTAL += 1
+                if not alarm_status:
+                    alarm_status = True
+                    t = Thread(target=alarm, args=('wake up sir',))
+                    t.deamon = True
+                    t.start()
+                if TOTAL > 20:
+                    cv2.putText(frame, "DROWSINESS ALERT!", (10, 30), cv2.FONT_HERSHEY_SIMPLEX, 0.7, (0, 0, 255), 2)
+
+        else:
+            COUNTER = 0
+            alarm_status = False
+
+        if distance > YAWN_THRESH:
+            cv2.putText(frame, "Yawn Alert", (10, 30),
+                        cv2.FONT_HERSHEY_SIMPLEX, 0.7, (0, 0, 255), 2)
+            if alarm_status2 == False and saying == False:
+                alarm_status2 = True
+                t = Thread(target=alarm, args=('take some fresh air',))
+                t.deamon = True
+                t.start()
+        else:
+            alarm_status2 = False
+
+        cv2.putText(frame, "EAR: {:.2f}".format(ear), (300, 30),
+                    cv2.FONT_HERSHEY_SIMPLEX, 0.7, (0, 0, 255), 2)
+        cv2.putText(frame, "YAWN: {:.2f}".format(distance), (300, 60),
+                    cv2.FONT_HERSHEY_SIMPLEX, 0.7, (0, 0, 255), 2)
+        cv2.putText(frame, "Blinks: {}".format(TOTAL), (10, 30), cv2.FONT_HERSHEY_SIMPLEX, 0.7, (0, 0, 255), 2)
+    cv2.imshow("Frame", frame)
+    key = cv2.waitKey(1) & 0xFF
+
+    if key == ord("q"):
+        break
+
+cv2.destroyAllWindows()
+vs.stop()
